c135df1fe6b3b040d3416cc4353fc0859e7a9784
GunmetalGears <mayordeepwater@gmail.com>
2017-05-18 10:39:07 -0700

    mercury bucket changes

--- src/main/java/com/gunmetal/smalladditions/SAItemManager.java
+++ src/main/java/com/gunmetal/smalladditions/SAItemManager.java
@@ -1,18 +1,13 @@
 package com.gunmetal.smalladditions;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.UniversalBucket;
 
-public class SAItemManager {
-	
+public class SAItemManager {	
 	public static MercuryDrop mercuryDrop = new MercuryDrop();
 	public static ItemBlock gStoneItem = new ItemBlock(SABlockManager.grassystone);
 	public static ItemBlock cOreItem = new ItemBlock(SABlockManager.orecinnabar);
-	public static UniversalBucket mercuryBucket = new UniversalBucket();
-	
-	public static void registerItems() {
-		mercuryBucket.setCreativeTab(Constants.SACTAB);
-		mercuryBucket.setRegistryName("mercurybucket");
-		mercuryBucket.getFilledBucket(mercuryBucket, SAFluidManager.fluidMercury);
-	}
+	public static UniversalBucket HgBucket = new UniversalBucket();
 }
