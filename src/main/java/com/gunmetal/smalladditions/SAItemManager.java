a99f51f3ee1c0318112c79ba51b534efa2d3d7ac
GunmetalGears <mayordeepwater@gmail.com>
2017-05-22 09:14:14 -0700

    some new stuff

--- src/main/java/com/gunmetal/smalladditions/SAItemManager.java
+++ src/main/java/com/gunmetal/smalladditions/SAItemManager.java
@@ -6,8 +6,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.UniversalBucket;
 
 public class SAItemManager {	
+	public static void registerItems() {
+		HgBucket = new UniversalBucket(1000, new ItemStack(Items.BUCKET), false);
+		HgBucket.setRegistryName("mercurybucket");
+		HgBucket.setUnlocalizedName("mercurybucket");
+	}
 	public static MercuryDrop mercuryDrop = new MercuryDrop();
 	public static ItemBlock gStoneItem = new ItemBlock(SABlockManager.grassystone);
 	public static ItemBlock cOreItem = new ItemBlock(SABlockManager.orecinnabar);
-	public static UniversalBucket HgBucket = new UniversalBucket(1000, new ItemStack(Items.BUCKET), false);
+	public static UniversalBucket HgBucket;
 }
